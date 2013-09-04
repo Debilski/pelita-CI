3c84c37055d30beaf1c97b22f34ccdf93d2e02d0
Rike-Benjamin Schuppner <rikebs@debilski.de>
2013-09-04 18:31:38 +0200

    WIP: Git pull

--- src/main/scala/de/debilski/pelita/pelitaci/frontend/snippet/Main.scala
+++ src/main/scala/de/debilski/pelita/pelitaci/frontend/snippet/Main.scala
@@ -2,3 +2,25 @@ package de.debilski.pelita.pelitaci.frontend
 package snippet
 
 
+import scala.xml.NodeSeq
+
+import net.liftweb.util.Helpers._
+import net.liftweb.http.SHtml
+import net.liftweb.http.js.{JsCmd, JsCmds}
+import net.liftweb.common.Loggable
+
+
+class GitRevision {
+  def currentRevision = try {
+    sys.process.Process( Seq( "git", "describe" ), new java.io.File( "/opt/player" )).!!
+  } catch {
+    case e: java.io.IOException => ""
+  }
+  def gitPull = {
+    sys.process.Process( Seq( "git", "describe" ), new java.io.File( "/opt/player" )).!!
+    JsCmds.Noop
+  }
+
+  def button = "button [onclick]" #> SHtml.ajaxInvoke(gitPull _)
+}
+
